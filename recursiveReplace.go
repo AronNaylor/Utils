eb237c08ddba6385570081168a2f414ac8e18e98
Aron Naylor <60755388+AronNaylor@users.noreply.github.com>
2023-08-01 14:48:48 +0100

    Update recursiveReplace.go

--- recursiveReplace.go
+++ recursiveReplace.go
@@ -1,36 +1,44 @@
-// RecursiveRepalce is th entrypoint for a recursive walk of an unmarshalld YAML document, if the type is not a string it recurses
-// using a ehlper function
-func RecursiveReplace(data interface{}, target string, replacement string) {
-	v := reflect.ValueOf(data)
-
-	switch v.Kind() {
-	case reflect.Slice:
-		for i := 0; i < v.Len(); i++ {
-			item := v.Index(i)
-			if item.Kind() == reflect.String {
-				if strings.Contains(item.String(), target) {
-					fmt.Println("Match found:", item.String())
+// ReplaceInMap is a helper function to replace complete strings and substrings in a map
+func ReplaceInMap(inputMap map[interface{}]interface{}, oldString, newString string) map[interface{}]interface{} {
+	for key, value := range inputMap {
+		switch concreteVal := value.(type) {
+		case map[interface{}]interface{}:
+			inputMap[key] = ReplaceInMap(concreteVal, oldString, newString)
+		case []interface{}:
+			for i, val := range concreteVal {
+				valMap, ok := val.(map[interface{}]interface{})
+				if ok {
+					concreteVal[i] = ReplaceInMap(valMap, oldString, newString)
+				} else {
+					strVal, ok := val.(string)
+					if ok {
+						concreteVal[i] = strings.ReplaceAll(strVal, oldString, newString)
+					}
 				}
-				v.Index(i).SetString(strings.ReplaceAll(item.String(), target, replacement))
-			} else {
-				RecursiveReplace(item.Interface(), target, replacement)
 			}
-		}
-	case reflect.Map:
-		for _, key := range v.MapKeys() {
-			value := v.MapIndex(key)
-			if value.Kind() == reflect.String {
-				if strings.Contains(value.String(), target) {
-					fmt.Println("Match found:", value.String())
-				}
-				v.SetMapIndex(key, reflect.ValueOf(strings.ReplaceAll(value.String(), target, replacement)))
-			} else {
-				RecursiveReplace(value.Interface(), target, replacement)
-			}
-		}
-	case reflect.String:
-		if strings.Contains(v.String(), target) {
-			strings.Replace(v.String(), target, replacement, -1)
+		case string:
+			inputMap[key] = strings.ReplaceAll(concreteVal, oldString, newString)
 		}
 	}
+	return inputMap
+}
+
+// RecursiveRepalce is the entrypoint for a recursive walk of an unmarshalld YAML document, if the type is not a string it recurses
+// using a helper function to find and replace strings
+func RecursiveReplace(yamlFile []byte, oldString, newString string) ([]byte, error) {
+	m := make(map[interface{}]interface{})
+
+	err := yaml.Unmarshal(yamlFile, &m)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling YAML: %w", err)
+	}
+
+	m = ReplaceInMap(m, oldString, newString)
+
+	newYaml, err := yaml.Marshal(m)
+	if err != nil {
+		return nil, fmt.Errorf("error marshalling YAML: %w", err)
+	}
+
+	return newYaml, nil
 }
